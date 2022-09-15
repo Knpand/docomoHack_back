52bd547300cda351d18cc315c05516332ff58a25
root <root@ip-172-31-2-114.ap-northeast-1.compute.internal>
2022-09-15 08:05:07 +0000

    create sql

--- docker/db/sqls/001-create-tables.sql
+++ docker/db/sqls/001-create-tables.sql
@@ -1,4 +1,10 @@
 create table students
-(id int not null auto_increment primary key, password varchar(128) not null, userid varchar(100) not null unique, name varchar(100))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+(id int not null auto_increment primary key, userid varchar(100) not null unique, password varchar(128) not null, name varchar(100))DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO students (password,userid, name) VALUES ("pass","id","hogeo");
+create table users
+(id int not null auto_increment primary key, user_id varchar(100) not null unique, user_name varchar(100),price int)DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
+
+INSERT INTO users (user_id, user_name,price) VALUES ("id","hogeo",100);
+INSERT INTO users (user_id, user_name,price) VALUES ("id2","hogehoge2",800);
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"net/http"
-
+	"log"
+	"fmt"
+	// "docomoHack_back/db"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
+	// "os"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+
 )
 
 type SearchRequest struct {
@@ -13,26 +20,111 @@ type SearchRequest struct {
 }
 
 type SearchResult struct {
-	StoreId   string `json:"store_id"`
-	StoreName string `json:"store_name"`
-	Result    bool   `json:"result"`
+	StoreIds   []string `json:"store_ids"`
+	StoreNames []string `json:"store_names"`
+	Result     bool     `json:"result"`
 }
+
+func sqlConnect() (database *gorm.DB, err error) {
+
+	DBMS := "mysql"
+	USER := "samp"
+	PASS := "samp"
+	PROTOCOL := "tcp(localhost:3306)"
+	DBNAME := "db"
+
+	log.Print(USER)
+	CONNECT := USER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?charset=utf8&parseTime=true&loc=Asia%2FTokyo"
+	return gorm.Open(DBMS, CONNECT)
+	
+
+}
+
+
+func connectDB() *gorm.DB {
+	// DB接続
+	db, err := sqlConnect()
+	if err != nil {
+		panic(err.Error())
+	} else {
+		fmt.Printf("Log: DBへの接続に成功しました in connectDB\n")
+	}
+
+	return db
+}
+
+
+func search(db *gorm.DB, CustomerId string, Latitude float64, Longitude float64) ([]string, []string, bool) {
+	var (
+		users      = make([]User, 0, 10)
+		storeIds   = make([]string, 0, 10)
+		storeNames = make([]string, 0, 10)
+	)
+	price := 800
+	if db.Find(&users, "price = ?", price); len(users) == 0 {
+		fmt.Printf("Error: 条件に一致するユーザがいません: %d in search\n", price)
+		return []string{}, []string{}, false
+	}
+
+	for _, s := range users {
+		storeIds = append(storeIds, s.UserId)
+		storeNames = append(storeNames, s.UserName)
+	}
+
+	return storeIds, storeNames, true
+}
+
+type StoreIdAndStoreName struct {
+	StoreId   string
+	StoreName string
+}
+type User struct {
+	UserId   string
+	UserName string
+	Price    int
+}
+
 
 func main() {
 	e := echo.New()
 	//e.GET("/", func(c echo.Context) error {
 	//	return c.String(http.StatusOK, "Hello, World!")
 	//})
-
+	godotenv.Load()
+	db := connectDB()
+	
 	e.POST("/search", func(c echo.Context) error {
 		request := new(SearchRequest)
 		err := c.Bind(request)
 		if err == nil {
+			//result := &SearchResult{
+			//Result: search(db, request.CustomerId, request.Latitude, request.Longitude),
+			//	StoreIds:   []string{"test_id"},
+			//	StoreNames: []string{"test_name"},
+			//	Result:    true,
+			//}
+			StoreIds, StoreNames, Result := search(db, request.CustomerId, request.Latitude, request.Longitude)
 			result := &SearchResult{
-				//Result: search(db, request.CustomerId, request.Latitude, request.Longitude),
-				StoreId:   "test_id",
-				StoreName: "test_name",
-				Result:    true,
+				StoreIds:   StoreIds,
+				StoreNames: StoreNames,
+				Result:     Result,
+			}
+
+			return c.JSON(http.StatusOK, result)
+		} else {
+			return c.JSON(http.StatusBadRequest, &SearchResult{Result: false})
+		}
+	})
+
+	e.GET("debug" , func(c echo.Context) error {
+		request := new(SearchRequest)
+		err := c.Bind(request)
+		if err == nil {
+			StoreIds, StoreNames, Result := search(db, request.CustomerId, request.Latitude, request.Longitude)
+			result := &SearchResult{
+				StoreIds:   StoreIds,
+				StoreNames: StoreNames,
+				Result:     Result,
 			}
 
 			return c.JSON(http.StatusOK, result)
