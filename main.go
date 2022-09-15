9ea71a0b1d78bb4d2d97a843956a080c06f24b91
bbbbbbbbbbbbb1 <ishikawa-yuya-nx@ynu.jp>
2022-09-15 12:37:30 +0900

    add: add echo template

--- main.go
+++ main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type SearchRequest struct {
+	CustomerId string  `json:"customer_id"`
+	Latitude   float64 `json:"latitude"`
+	Longitude  float64 `json:"longitude"`
+}
+
+type SearchResult struct {
+	StoreId   string `json:"store_id"`
+	StoreName string `json:"store_name"`
+	Result    bool   `json:"result"`
+}
 
 func main() {
-	fmt.Printf("Hello World\n")
+	e := echo.New()
+	//e.GET("/", func(c echo.Context) error {
+	//	return c.String(http.StatusOK, "Hello, World!")
+	//})
+
+	e.POST("/search", func(c echo.Context) error {
+		request := new(SearchRequest)
+		err := c.Bind(request)
+		if err == nil {
+			result := &SearchResult{
+				//Result: search(db, request.CustomerId, request.Latitude, request.Longitude),
+				StoreId:   "test_id",
+				StoreName: "test_name",
+				Result:    true,
+			}
+
+			return c.JSON(http.StatusOK, result)
+		} else {
+			return c.JSON(http.StatusBadRequest, &SearchResult{Result: false})
+		}
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
