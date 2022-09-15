f41e88951404522e1fbb37800c1feaa72ad33300
bbbbbbbbbbbbb1 <ishikawa-yuya-nx@ynu.jp>
2022-09-15 20:01:26 +0900

    add: add api

--- db.go
+++ db.go
@@ -3,19 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
-type StoreIdAndStoreName struct {
-	StoreId   string
-	StoreName string
-}
 type User struct {
 	UserId   string
 	UserName string
 	Price    int
+}
+
+type Customer struct {
+	CustomerId   string
+	Password     string
+	Gender       string
+	CustomerName string
+	Address      string
+	Birth        time.Time
+	Rool         string
+}
+
+type Store struct {
+	StoreId   string
+	Password  string
+	StoreName string
+	Category  string
+	Latitude  float64
+	Longitude float64
+	Address   string
+	Price     int
+}
+
+type Term struct {
+	StoreId string
+	Gender  string
+	MinAge  int
+	MaxAge  int
+	Role    string
 }
 
 func sqlConnect() (database *gorm.DB, err error) {
@@ -59,4 +85,104 @@ func search(db *gorm.DB, CustomerId string, Latitude float64, Longitude float64)
 	}
 
 	return storeIds, storeNames, true
+}
+
+func book(db *gorm.DB, CustomerId string, StoreId string) bool {
+	return true
+}
+
+func customerSignup(db *gorm.DB, CustomerId string, CustomerName string, Gender string, Address string, Birth time.Time, Rool string, Password string) bool {
+	var customer Customer
+
+	if d_err := db.First(&customer, "customer_id = ?", CustomerId).Error; d_err != nil {
+		customer = Customer{
+			CustomerId:   CustomerId,
+			CustomerName: CustomerName,
+			Gender:       Gender,
+			Address:      Address,
+			Birth:        Birth,
+			Rool:         Rool,
+			Password:     Password,
+		}
+		if customer_err := db.Create(&customer).Error; customer_err == nil {
+			fmt.Printf("Log: 顧客情報を登録: %s in customerSignup\n", CustomerId)
+			return true
+		} else {
+			fmt.Printf("Error: 顧客情報の登録に失敗しました: %s in customerSignup\n", CustomerId)
+			return false
+		}
+	} else {
+		fmt.Printf("Error: そのIDは既に使われています: %s in customerSignup\n", CustomerId)
+		return false
+	}
+}
+
+func storeSignup(db *gorm.DB, StoreId string, StoreName string, Category string, Address string, Latitude float64, Longitude float64, Price int, Password string, Gender string, MinAge int, MaxAge int, Role string) bool {
+	var (
+		store Store
+		term  Term
+	)
+
+	if d_err := db.First(&store, "store_id = ?", StoreId).Error; d_err != nil {
+		store = Store{
+			StoreId:   StoreId,
+			Password:  Password,
+			StoreName: StoreName,
+			Category:  Category,
+			Latitude:  Latitude,
+			Longitude: Longitude,
+			Address:   Address,
+			Price:     Price,
+		}
+		if store_err := db.Create(&store).Error; store_err == nil {
+			fmt.Printf("Log: 飲食店の情報を登録: %s in storeSignup\n", StoreId)
+			term = Term{
+				StoreId: StoreId,
+				Gender:  Gender,
+				MinAge:  MinAge,
+				MaxAge:  MaxAge,
+				Role:    Role,
+			}
+			if term_err := db.Create(&term).Error; term_err == nil {
+				fmt.Printf("Log: 飲食店の顧客条件情報を登録: %s in storeSignup\n", StoreId)
+				return true
+			} else {
+				fmt.Printf("Error: 飲食店の顧客条件情報の登録に失敗しました: %s in storeSignup\n", StoreId)
+				return false
+			}
+		} else {
+			fmt.Printf("Error: 飲食店の情報の登録に失敗しました: %s in storeSignup\n", StoreId)
+			return false
+		}
+	} else {
+		fmt.Printf("Error: そのIDは既に使われています: %s in storeSignup\n", StoreId)
+		return false
+	}
+}
+
+func termUpdate(db *gorm.DB, StoreId string, Gender string, MinAge int, MaxAge int, Role string) bool {
+	var (
+		term Term
+	)
+
+	if err := db.Model(&term).Where("store_id = ?", StoreId).Updates(map[string]interface{}{"gender": Gender, "min_age": MinAge, "max_age": MaxAge, "role": Role}).Error; err != nil {
+		fmt.Printf("Error:  飲食店の顧客条件情報の更新に失敗しました: %s in termUpdate\n", StoreId)
+		return false
+	}
+
+	fmt.Printf("Log: 飲食店の顧客条件情報を更新: %s in termUpdate\n", StoreId)
+	return true
+}
+
+func login(db *gorm.DB, UserId string, Password string) bool {
+	var customer Customer
+	if customer_err := db.First(&customer, "customer_id = ? AND password = ?", UserId, Password).Error; customer_err != nil {
+		var store Store
+		if store_err := db.First(&store, "store_id = ? AND password = ?", UserId, Password).Error; store_err != nil {
+			fmt.Printf("Error: ログインに失敗しました: %s in login\n", UserId)
+			return false
+		}
+	}
+
+	return true
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
@@ -17,6 +18,69 @@ type SearchResult struct {
 	StoreIds   []string `json:"store_ids"`
 	StoreNames []string `json:"store_names"`
 	Result     bool     `json:"result"`
+}
+
+type BookRequest struct {
+	CustomerId string `json:"customer_id"`
+	StoreId    string `json:"store_id"`
+}
+
+type BookResult struct {
+	Result bool `json:"result"`
+}
+
+type CustomerSignupRequest struct {
+	CustomerId   string    `json:"customer_id"`
+	CustomerName string    `json:"customer_name"`
+	Gender       string    `json:"gender"`
+	Address      string    `json:"address"`
+	Birth        time.Time `json:"birth"`
+	Role         string    `json:"rool"`
+	Password     string    `json:"password"`
+}
+
+type CustomerSignupResult struct {
+	Result bool `json:"result"`
+}
+
+type StoreSignupRequest struct {
+	StoreId   string  `json:"store_id"`
+	StoreName string  `json:"store_name"`
+	Category  string  `json:"category"`
+	Address   string  `json:"address"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Price     int     `json:"price"`
+	Password  string  `json:"password"`
+	Gender    string  `json:"gender"`
+	MinAge    int     `json:"min_age"`
+	MaxAge    int     `json:"max_age"`
+	Role      string  `json:"role"`
+}
+
+type StoreSignupResult struct {
+	Result bool `json:"result"`
+}
+
+type TermUpdateRequest struct {
+	StoreId string `json:"store_id"`
+	Gender  string `json:"gender"`
+	MinAge  int    `json:"min_age"`
+	MaxAge  int    `json:"max_age"`
+	Role    string `json:"role"`
+}
+
+type TermUpdateResult struct {
+	Result bool `json:"result"`
+}
+
+type LoginRequest struct {
+	UserId   string `json:"user_id"`
+	Password string `json:"password"`
+}
+
+type LoginResult struct {
+	Result bool `json:"result"`
 }
 
 func main() {
@@ -47,6 +111,81 @@ func main() {
 			return c.JSON(http.StatusOK, result)
 		} else {
 			return c.JSON(http.StatusBadRequest, &SearchResult{Result: false})
+		}
+	})
+
+	e.POST("/book", func(c echo.Context) error {
+		request := new(BookRequest)
+		err := c.Bind(request)
+		if err == nil {
+			Result := book(db, request.CustomerId, request.StoreId)
+			result := &BookResult{
+				Result: Result,
+			}
+
+			return c.JSON(http.StatusOK, result)
+		} else {
+			return c.JSON(http.StatusBadRequest, &BookResult{Result: false})
+		}
+	})
+
+	e.POST("/customersignup", func(c echo.Context) error {
+		request := new(CustomerSignupRequest)
+		err := c.Bind(request)
+		if err == nil {
+			Result := customerSignup(db, request.CustomerId, request.CustomerName, request.Gender, request.Address, request.Birth, request.Role, request.Password)
+			result := &CustomerSignupResult{
+				Result: Result,
+			}
+
+			return c.JSON(http.StatusOK, result)
+		} else {
+			return c.JSON(http.StatusBadRequest, &CustomerSignupResult{Result: false})
+		}
+	})
+
+	e.POST("/storesignup", func(c echo.Context) error {
+		request := new(StoreSignupRequest)
+		err := c.Bind(request)
+		if err == nil {
+			Result := storeSignup(db, request.StoreId, request.StoreName, request.Category, request.Address, request.Latitude, request.Longitude, request.Price, request.Password, request.Gender, request.MinAge, request.MaxAge, request.Role)
+			result := &StoreSignupResult{
+				Result: Result,
+			}
+
+			return c.JSON(http.StatusOK, result)
+		} else {
+			return c.JSON(http.StatusBadRequest, &StoreSignupResult{Result: false})
+		}
+	})
+
+	e.POST("/termupdate", func(c echo.Context) error {
+		request := new(TermUpdateRequest)
+		err := c.Bind(request)
+		if err == nil {
+			Result := termUpdate(db, request.StoreId, request.Gender, request.MinAge, request.MaxAge, request.Role)
+			result := &TermUpdateResult{
+				Result: Result,
+			}
+
+			return c.JSON(http.StatusOK, result)
+		} else {
+			return c.JSON(http.StatusBadRequest, &TermUpdateResult{Result: false})
+		}
+	})
+
+	e.POST("/login", func(c echo.Context) error {
+		request := new(LoginRequest)
+		err := c.Bind(request)
+		if err == nil {
+			Result := login(db, request.UserId, request.Password)
+			result := &LoginResult{
+				Result: Result,
+			}
+
+			return c.JSON(http.StatusOK, result)
+		} else {
+			return c.JSON(http.StatusBadRequest, &LoginResult{Result: false})
 		}
 	})
 
